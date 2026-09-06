6e113b635440a2aa48231a1d30b9f8d93035d0fa
diarra0201 <mamediarradiane93@gmail.com>
2026-09-06 20:28:28 +0000

    fix(docker): passage du port PostgreSQL Docker vers 5433

--- backend/mbeund_mi/settings.py
+++ backend/mbeund_mi/settings.py
@@ -65,7 +65,7 @@ DATABASES = {
         'USER': os.environ.get('DB_USER', 'postgres'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'mbeund_mi_password'),
         'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'PORT': os.environ.get('DB_PORT', '5433'),
     }
 }
 
